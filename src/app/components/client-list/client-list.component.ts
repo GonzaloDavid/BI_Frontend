867375ea68863f08ce0caa2d71057472775e7036
David Proaño <davidmac@Mac-Studio-de-David.local>
2023-12-12 00:29:38 -0500

    Nuevo componente de listado de personas

--- src/app/components/client-list/client-list.component.ts
+++ src/app/components/client-list/client-list.component.ts
@@ -1,12 +1,20 @@
 import { Component } from '@angular/core';
+import { TableModule } from 'primeng/table';
 
 @Component({
   selector: 'app-client-list',
   standalone: true,
-  imports: [],
+  imports: [TableModule],
   templateUrl: './client-list.component.html',
   styleUrl: './client-list.component.scss'
 })
 export class ClientListComponent {
+
+  clientList:any[];
+
+  constructor()
+  {
+    this.clientList=[];
+  }
 
 }
